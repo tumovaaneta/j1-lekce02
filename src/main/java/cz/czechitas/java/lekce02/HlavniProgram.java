af5ae9322bb3341a6c6b51c13dde2c47bd7e5481
Aneta <tumova-aneta@seznam.cz>
2023-02-14 20:23:41 +0100

    Zofka - 2.verze

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -7,48 +7,64 @@ public class HlavniProgram {
     public void start() {
         zofka = new Turtle();
 
-    kresliCtverec();
+        kresliCtverec();
 
-    zofka.penUp();
-    zofka.move(200);
-    zofka.penDown();
-    zofka.turnLeft(45);
-    zofka.move(145);
-    zofka.turnLeft(90);
-    zofka.move(145);
+        kresliStrechu();
 
-    zofka.penUp();
-    zofka.turnLeft(45);
-    zofka.move(300);
-    zofka.penDown();
+        zofka.penUp();
+        zofka.turnLeft(45);
+        zofka.move(300);
+        zofka.penDown();
+
+        nakresliPrase();
+
+
+    }
+
+    private void nakresliPrase() {
         for (int i = 0; i < 2; i++) {
             zofka.move(100);
             zofka.turnLeft(90);
             zofka.move(200);
             zofka.turnLeft(90);
         }
-    zofka.turnRight(45);
-    zofka.move(75);
-    zofka.turnLeft(90);
-    zofka.move(125);
-    zofka.turnLeft(180);
-    zofka.move(50);
-    zofka.turnLeft(90);
-    zofka.move(50);
-    zofka.turnLeft(180);
-    zofka.move(50);
-    zofka.turnRight(45);
-    zofka.penUp();
-    zofka.move(200);
-    zofka.turnRight(45);
-    zofka.penDown();
-    zofka.move(50);
-    zofka.turnLeft(180);
-    zofka.move(50);
-    zofka.turnLeft(90);
-    zofka.move(50);
-
+        zofka.turnRight(45);
+        zofka.move(75);
+        zofka.turnLeft(90);
+        zofka.move(125);
+        zofka.turnLeft(180);
+        zofka.move(50);
+        zofka.turnLeft(90);
+        zofka.move(50);
+        zofka.turnLeft(180);
+        zofka.move(50);
+        zofka.turnRight(45);
+        zofka.penUp();
+        zofka.move(200);
+        zofka.turnRight(45);
+        zofka.penDown();
+        zofka.move(50);
+        zofka.turnLeft(180);
+        zofka.move(50);
+        zofka.turnLeft(90);
+        zofka.move(50);
+        zofka.turnRight(180);
+        zofka.move(50);
+        zofka.turnLeft(45);
+        zofka.move(100);
     }
+
+    private void kresliStrechu() {
+        zofka.penUp();
+        zofka.move(200);
+        zofka.penDown();
+        zofka.turnLeft(45);
+        zofka.move(145);
+        zofka.turnLeft(90);
+        zofka.move(145);
+    }
+
+}
 
     private void kresliCtverec() {
         for (int i = 0; i < 4 ; i++) {
